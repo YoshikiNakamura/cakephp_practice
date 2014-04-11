c04eab21de0fe38569548f403b51e37fe3865dd1
Yoshiki <yn.phy6@gmail.com>
2014-04-11 13:29:48 +0900

    md database.php for MAMP

--- app/Config/database.php
+++ app/Config/database.php
@@ -1,42 +1,42 @@
 <?php
 
-define('RDS_HOSTNAME', $_SERVER['RDS_HOSTNAME']);
-define('RDS_USERNAME', $_SERVER['RDS_USERNAME']);
-define('RDS_PASSWORD', $_SERVER['RDS_PASSWORD']);
-define('RDS_DB_NAME', $_SERVER['RDS_DB_NAME']);
+//MAMP用
+if ($_SERVER['HTTP_HOST'] === 'localhost') {
+	define('RDS_HOSTNAME', 'localhost');
+	define('RDS_USERNAME', 'root');
+	define('RDS_PASSWORD', 'root');
+	define('RDS_DB_NAME', 'cakephp-practice');
+}
 
-class DATABASE_CONFIG
-{
+//AWS用
+if (!defined('RDS_HOSTNAME')) {
+	define('RDS_HOSTNAME', $_SERVER['RDS_HOSTNAME']);
+	define('RDS_USERNAME', $_SERVER['RDS_USERNAME']);
+	define('RDS_PASSWORD', $_SERVER['RDS_PASSWORD']);
+	define('RDS_DB_NAME', $_SERVER['RDS_DB_NAME']);
+}
+
+class DATABASE_CONFIG {
+
 	public $default = array(
-		'datasource' => 'Database/Mysql',
-		'persistent' => false,
-		'host' => RDS_HOSTNAME,
-		'login' => RDS_USERNAME,
-		'password' => RDS_PASSWORD,
-		'database' => RDS_DB_NAME,
-		'prefix' => '',
-		'encoding' => 'utf8',
+			'datasource' => 'Database/Mysql',
+			'persistent' => false,
+			'host' => RDS_HOSTNAME,
+			'login' => RDS_USERNAME,
+			'password' => RDS_PASSWORD,
+			'database' => RDS_DB_NAME,
+			'prefix' => '',
+			'encoding' => 'utf8'
 	);
 
 	public $test = array(
-		'datasource' => 'Database/Mysql',
-		'persistent' => false,
-		'host' => RDS_HOSTNAME,
-		'login' => RDS_USERNAME,
-		'password' => RDS_PASSWORD,
-		'database' => RDS_DB_NAME,
-		'prefix' => '',
-		'encoding' => 'utf8',
+			'datasource' => 'Database/Mysql',
+			'persistent' => false,
+			'host' => RDS_HOSTNAME,
+			'login' => RDS_USERNAME,
+			'password' => RDS_PASSWORD,
+			'database' => RDS_DB_NAME,
+			'prefix' => '',
+			'encoding' => 'utf8'
 	);
-
-	public function __construct()
-	{
-		if ($_SERVER['HTTP_HOST'] === 'localhost')
-		{
-			$this->default['host'] = 'localhost';
-			$this->default['login'] = 'root';
-			$this->default['password'] = 'root';
-			$this->default['database'] = 'cakephp-phpunit-guzzle';
-		}
-	}
 }
